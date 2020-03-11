8d43b3503cfd9b15e71333c62480c9676f5d766a
prazdnikovamargarita <59223407+prazdnikovamargarita@users.noreply.github.com>
2020-03-11 23:40:42 +0200

    Fixed Files

--- homework.py
+++ homework.py
@@ -11,129 +11,63 @@ class OurAwesomeException(Exception):
 
 
 def is_two_object_has_same_value(first: Any, second: Any) -> bool:
-    if (first == second):
-        result = True
-    else:
-        result = False
-    print (result)
-    return result
+    return first == second
 
-    pass
+    
 
 
 def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
-    type_first= (type(first))
-    type_second = (type(second))
-    if(type_first == type_second):
-        result= True
-    else:
-        result= False
-    print (result)
-    return result
-    pass
+
+    return type(first) == type(second)
+    
 
 
 def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
-    if (first is second):
-        result = True
-    else:
-        result = False
-    return result
 
-    pass
-    """
-    If @first and @second has same type should return True
-    In another case should return False
-    """
-    pass
+    return first is second
+
 
 
 def multiple_ints(first_value: int, second_value: int) -> int:
+    if(isinstance(first_value, bool)  & isinstance(second_value, bool) ):
+        raise TypeError
    
-    if((type(first_value) is int) & (type(second_value) is int)):
+    elif (isinstance(first_value, int)  & isinstance(second_value, int) ):
         return first_value * second_value
     else:
         raise TypeError
     
-    pass
+    
 
 
 def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
     try:
-        first_float = float(first_value)
-        second_float = float(second_value)   
-        first_int = int (first_float)
-        second_int = int(second_float)
-        return first_int * second_int
-    except:
+        return int (first_value) * int(second_value)
+    except (ValueError, TypeError):
         raise ValueError
         
     
-    pass
 
 
 def is_word_in_text(word: str, text: str) -> bool:
-    if (word in text):
-        result = True
-        return True
-    else:
-        result = True
-        return False
 
-    print (result)
-    return result
-    pass
+    return word in text
 
 
 def some_loop_exercise() -> list:
-    result =[]
-    for i in range(0,13):
-        if ((i!=6) and (i!=7)):
-            result.append(i)
-    return result
-    """
-    if ((i!=6) & (i!=7)):
-        result = [i for i in range(1,12)]
-    """
     
-    pass
+    return [i for i in range(0,13) if ((i!=6) & (i!=7))]
+    
 
 
 def remove_from_list_all_negative_numbers(data: List[int]) -> list:
-
-    result =[]
-    for i in range(len(data)):
-        result.append(data[i])
-        if (data[i]<0):
-            result.remove(data[i])
-    print(result)
-    return result
-    
-    """
-    if ((i>=0)):
-        result = [i for i in range(len(list)-1)]
-    """
-    """
-a=-1000
-    b=1000
-    result =[]
-    for i in range(len(list)-1):
-        if (i>=0):
-            result.append(i)
-    print(result)
-    """
-
-    pass
+  
+    return [data[i] for i in range(len(data)) if (data[i]>0)]
 
 
 def alphabet() -> dict:
-    alph = {
-        1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l',
-        13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w',
-        24: 'x', 25: 'y', 26: 'z'
-    }
-    print(alph)
-    return alph 
+
+    return {i : chr(i + 96) for i in range (1,27)} 
     """
     Create dict which keys are alphabetic characters. And values their number in alphabet
     Notes You could see an implementaion of this one in test, but create another one
@@ -141,7 +75,6 @@ def alphabet() -> dict:
         alphabet()
         >>> {"a": 1, "b": 2 ...}
     """
-    pass
 
 
 def simple_sort(data: List[int]) -> List[list]:
@@ -159,4 +92,4 @@ def simple_sort(data: List[int]) -> List[list]:
         simple_sort([2, 9, 6, 7, 3, 2, 1])
         >>> [1, 2, 2, 3, 6, 7, 9]
     """
-    pass
+    
