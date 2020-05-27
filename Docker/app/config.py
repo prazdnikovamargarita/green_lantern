9e073fe81a8ac129365b29d21add82bfccadb57b
prazdnikovamargarita <59223407+prazdnikovamargarita@users.noreply.github.com>
2020-05-27 17:47:31 +0300

    oops

--- Docker/app/config.py
+++ Docker/app/config.py
@@ -2,7 +2,7 @@
 class Config:
     PG_USER = "cursor"
     PG_PASSWORD = "very_secret_password"
-    PG_HOST = "localhost"
+    PG_HOST = "db"
     PG_PORT = 5432
     DB_NAME = "cursor_sqlalchemy_db"
     SQLALCHEMY_DATABASE_URI = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{DB_NAME}"
